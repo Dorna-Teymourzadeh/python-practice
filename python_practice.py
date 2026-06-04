0a53548a88e5f505edfde95de59cf4a1ec145e15
DORNA <dornateymourzadeh@Dornas-MacBook-Air.local>
2026-06-04 13:16:25 +0200

    Learned list copying

--- python_practice.py
+++ python_practice.py
@@ -57,3 +57,32 @@ areas_2 = areas_1+['garage',15.45]
 
 print(areas_1)
 print(areas_2)
+
+areas = ["hallway", 11.25, "kitchen", 18.0,
+        "chill zone", 20.0, "bedroom", 10.75,
+         "bathroom", 10.50, "poolhouse", 24.5,
+         "garage", 15.45]
+
+# Delete the poolhouse items from the list
+del areas[10]
+del areas[10]
+
+
+
+# Print the updated list
+print (areas)
+
+# Create list areas
+areas = [11.25, 18.0, 20.0, 10.75, 9.50]
+
+# Change this command
+areas_copy = areas
+areas_copy= areas [:]
+
+# Change areas_copy
+areas_copy[0] = 5.0
+areas_copy[0] = 5.0
+
+# Print areas
+print(areas)
+print (areas_copy)
